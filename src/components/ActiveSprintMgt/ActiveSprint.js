532c6c0feb3e011dfae98586720e38fb0219c35f
Dulani Lamahewage <95127130+lamahewageDulani@users.noreply.github.com>
2023-03-25 22:02:35 +0530

    Completed

--- src/components/ActiveSprintMgt/ActiveSprint.js
+++ src/components/ActiveSprintMgt/ActiveSprint.js
@@ -3,6 +3,8 @@ import Sidebar from '../SideBar/Sidebar'
 import './ActiveSprint.css'
 
 
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
+
 const ActiveSprint = () => {
   const [inactive, setInactive] = React.useState(false);
  
@@ -20,6 +22,29 @@ const ActiveSprint = () => {
         <div>
           <div className="activeSprint">
             <h2>Active Sprint</h2>
+            <div className='dropDowns'>
+              <select>
+                <option>Team 1</option>
+                <option>Team 2</option>
+              </select>
+              <select>
+                <option>Sprint 1</option>
+                <option>Sprint 2</option>
+                <option>Sprint 3</option>
+                <option>Sprint 4</option>
+              </select> 
+            </div> 
+          </div>
+          <div className='table'>
+            <div className='columnTopic'>TO DO
+              <div>
+                <button className='issues'>Issue 1</button>
+              </div>
+            </div>
+            <div className='columnTopic'>IN PROGRESS</div>
+            <div className='columnTopic'>READY FOR QA</div>
+            <div className='columnTopic'>QA IN PROGRSS</div>
+            <div className='columnTopic'>DONE</div>
           </div>
         </div>
       </div>
@@ -27,4 +52,4 @@ const ActiveSprint = () => {
   )
 }
 
-export default ActiveSprint;
+export default ActiveSprint
